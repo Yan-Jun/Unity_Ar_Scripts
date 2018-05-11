e8db6722d186df771717e1517b9eca0d685622c7
Yan-Jun <s14113136@stu.edu.tw>
2018-05-11 19:07:23 +0800

    Update CameraGyro.cs

--- CameraGyro.cs
+++ CameraGyro.cs
@@ -7,6 +7,8 @@ public class CameraGyro : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Input.gyro.enabled = true;
+	Input.compensateSensors = true;		// Compensated sensors are accelerometer, compass, gyroscope.
+	Input.gyro.updateInterval = 0.01f;	// Update gyroscope data interval
     }
 	
 	// Update is called once per frame
