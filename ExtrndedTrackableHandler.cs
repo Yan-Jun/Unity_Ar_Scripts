6cbfa591db5e59a7ae319353f49aeb0ef6c7b5fb
Yan-Jun <s14113136@stu.edu.tw>
2018-05-11 19:09:56 +0800

    Add Extended Trackable

---
+++ ExtrndedTrackableHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using Vuforia;
+
+public class ExtrndedTrackableHandler : MonoBehaviour
+{
+    [SerializeField]
+    private TrackableBehaviour _trackableBehaviour;
+
+    private void Start()
+    {
+
+    }
+
+    public void OnEnableExtendeTracking()
+    {
+        ObjectTracker tracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+        tracker.Stop();
+        ((ImageTarget)_trackableBehaviour.Trackable).StartExtendedTracking();
+        tracker.Start();
+    }
+
+    public void OnDisableExtendeTracking()
+    {
+
+        ObjectTracker tracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+        tracker.Stop();
+        ((ImageTarget)_trackableBehaviour.Trackable).StopExtendedTracking();
+        tracker.Start();
+    }
+
+}
