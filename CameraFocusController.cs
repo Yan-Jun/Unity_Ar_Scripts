ea478fdef3ec319e451a84156aacfa7ce9726afd
Yan-Jun <s14113136@stu.edu.tw>
2018-05-11 21:43:02 +0800

    Upgrade CameraFocusController.cs

--- CameraFocusController.cs
+++ CameraFocusController.cs
@@ -4,28 +4,46 @@ using Vuforia;
 
 public class CameraFocusController : MonoBehaviour {
 
-	public static bool m_bIsFocus;
+	[SerializeField]
+    private CameraDevice.FocusMode FocusMode = CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO;
 
-	// Use this for initialization
-	void Start()
-	{
-		m_bIsFocus = false;
-		//CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
-		CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_TRIGGERAUTO);
-	}
+    void Start()
+    {
+        // Save player prefs focus mode.
+        FocusMode = (CameraDevice.FocusMode)PlayerPrefs.GetInt(TT_Manager.KEY_FOCUSMODE, (int)CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
 
-	// Update is called once per frame
-	void Update()
-	{
-		//if (m_bIsFocus)
-		#if UNITY_EDITOR
-			if(Input.GetMouseButtonUp(0))
-		#elif UNITY_ANDROID || UNITY_IPHONE
-			if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-		#endif
-		{
-			//CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
-			CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_TRIGGERAUTO);
-		}
-	}
+        var vuforia = VuforiaARController.Instance;
+        vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+        vuforia.RegisterOnPauseCallback(OnPaused);
+
+        // Trigger auto focus mode.
+        if(FocusMode == CameraDevice.FocusMode.FOCUS_MODE_TRIGGERAUTO)
+            vuforia.RegisterTrackablesUpdatedCallback(OnTriggerAutoUpdate);
+    }
+
+    private void OnTriggerAutoUpdate()
+    {
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonUp(0))
+#elif UNITY_ANDROID || UNITY_IPHONE
+	if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+#endif
+        {
+            CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_TRIGGERAUTO);
+        }
+    }
+
+    private void OnVuforiaStarted()
+    {
+        CameraDevice.Instance.SetFocusMode(FocusMode);
+    }
+
+    private void OnPaused(bool paused)
+    {
+        if (!paused) // resumed
+        {
+            // Set again autofocus mode when app is resumed
+            CameraDevice.Instance.SetFocusMode(FocusMode);
+        }
+    }
 }
